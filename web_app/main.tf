388b8e251f2093759c37aeeb331fef6321fd1d34
Aditya Bellary <adityabellary71@gmail.com>
2024-05-21 10:20:14 +0530

    changes to main.tf

--- web_app/main.tf
+++ web_app/main.tf
@@ -10,12 +10,14 @@ resource "aws_subnet" "subnet1" {
     vpc_id = aws_vpc.main.id
     cidr_block = var.subnet1_cidr
     availability_zone = var.az1
+    map_public_ip_on_launch = true
 }
 
 resource "aws_subnet" "subnet2" {
     vpc_id = aws_vpc.main.id
     cidr_block = var.subnet2_cidr
     availability_zone = var.az2
+    map_public_ip_on_launch = true
 }
 
 resource "aws_internet_gateway" "igw" {
